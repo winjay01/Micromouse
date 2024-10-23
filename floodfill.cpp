04e2e93a04fd4b9b0ea0470530b38c1c5ffcefee
James Wingard <wingarj1@tcnj.edu>
2024-10-23 03:00:07 -0400

    Floodfill

---
+++ floodfill.cpp
@@ -0,0 +1,81 @@
+#include "myQueue.hpp"
+using namespace std;
+
+// Maze representation: 16x16 unit grid
+int wall[16][16]; // wall[i][j] = 0001 means cell(i,j) has a wall on its left side only (clockwise, start at top)
+int path_cost[16][16]; // path_cost[i][j] = k means cell (i,j) has a path cost of k to the 4 center cells
+const int CLEAR = 64;
+
+arrayQueue<int> q;
+
+void clear_costs() {
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            path_cost[i][j] = CLEAR; // just a number to indicate a cleared path cost; 0 is used for center cells
+            //wall[i][j] = 0;
+        }
+    }
+}
+
+void print_costs() {
+    for (int i = 0; i < 16; i++) {
+        for (int j = 0; j < 16; j++) {
+            printf("%d ", path_cost[i][j]);
+            if (path_cost[i][j] <= 9) printf(" ");
+        }
+        printf("\n");
+    }
+}
+
+void check_neighbors(int current_cell) {
+    int row = current_cell / 16;
+    int col = current_cell % 16;
+    
+    // Check each direction
+    // Up
+    if ( (path_cost[row + 1][col] == CLEAR) && !(wall[row][col] & (1 << 3)) ) {
+        path_cost[row + 1][col] = path_cost[row][col] + 1;
+        q.enQueue((row+1)*16 + col);
+    }
+    // Right
+    if ( (path_cost[row][col + 1] == CLEAR) && !(wall[row][col] & (1 << 2)) ) {
+        path_cost[row][col + 1] = path_cost[row][col] + 1;
+        q.enQueue(row*16 + (col + 1));
+    }
+    // Down
+    if ( (path_cost[row - 1][col] == CLEAR) && !(wall[row][col] & (1 << 1)) ) {
+        path_cost[row - 1][col] = path_cost[row][col] + 1;
+        q.enQueue((row-1)*16 + col);
+    }
+    // Left
+    if ( (path_cost[row ][col - 1] == CLEAR) && !(wall[row][col] & (1 << 0)) ) {
+        path_cost[row][col - 1] = path_cost[row][col] + 1;
+        q.enQueue(row*16 + (col - 1));
+    }
+    return;
+}
+
+int main() {
+    clear_costs();
+    // Set path cost of center cells to 0
+    path_cost[16/2 - 1][16/2 - 1] = 0;
+    path_cost[16/2 - 1][16/2] = 0;
+    path_cost[16/2][16/2 - 1] = 0;
+    path_cost[16/2][16/2] = 0;
+    
+    // Cell (row,col) given a number n is (n/16, n%16) ; n = row*16 + col
+    q.enQueue(7*16 + 7);
+    q.enQueue(7*16 + 8);
+    q.enQueue(8*16 + 7);
+    q.enQueue(8*16 + 8);
+
+    // S1
+    int current_cell;
+    do {
+        current_cell = q.front();
+        q.deQueue();
+        check_neighbors(current_cell);
+    } while(!q.empty());
+
+    print_costs();
+}
