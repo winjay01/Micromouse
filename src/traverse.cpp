51b0cf52920b7dfd4ca30cc2c06436e9fb6ec3cf
James Wingard <wingarj1@tcnj.edu>
2024-10-28 17:47:43 -0400

    Added sensor decoding to enable mouse to add detected walls to its knowledge of the maze

--- src/traverse.cpp
+++ src/traverse.cpp
@@ -2,10 +2,48 @@
 using namespace std;
 
 int path[256]; // save path traversed
+int curr_dir; // which way are we facing with respect to center
+const int NORTH = 0;
+const int EAST = 1;
+const int SOUTH = 2;
+const int WEST = 3;
+// Sensor directions
+const int FORWARD = 0;
+const int LEFT = 1;
+const int RIGHT = 2;
 
-// Add detected wall to represntation of maze layout
+
+// Add detected wall to representation of maze layout
 void update_layout(int cell1, int cell2) {
     wall[cell1][cell2] = 1;
+}
+
+// Figure out where in maze the detected wall is
+void wall_int(int current_cell, int dir, int sensor_dir) {
+    if (sensor_dir == FORWARD) {
+        switch (dir) {
+            case NORTH: update_layout(current_cell, current_cell - 16); break;
+            case EAST: update_layout(current_cell, current_cell + 1); break;
+            case SOUTH: update_layout(current_cell, current_cell + 16); break;
+            case WEST: update_layout(current_cell, current_cell - 1); break;
+        }
+    }
+    else if (sensor_dir == LEFT) {
+        switch (dir) {
+            case NORTH: update_layout(current_cell, current_cell - 1); break;
+            case EAST: update_layout(current_cell, current_cell - 16); break;
+            case SOUTH: update_layout(current_cell, current_cell + 1); break;
+            case WEST: update_layout(current_cell, current_cell + 16); break;
+        }
+    }
+    else if (sensor_dir == RIGHT) {
+        switch (dir) {
+            case NORTH: update_layout(current_cell, current_cell + 1); break;
+            case EAST: update_layout(current_cell, current_cell + 16); break;
+            case SOUTH: update_layout(current_cell, current_cell - 1); break;
+            case WEST: update_layout(current_cell, current_cell - 16); break;
+        }
+    }
 }
 
 int in_path(int cell) {
@@ -36,8 +74,8 @@ int center_reached(int current_cell) {
     return ((current_cell == (7*16 + 7)) || (current_cell == (7*16 + 8)) || (current_cell == (8*16 + 7)) || (current_cell == (8*16 + 8)));
 }
 
+// Find adjacent cell with lowest path cost
 int min(int row, int col) {
-    //printf("Checking min...\n");
     int min = 256;
     int min_cell = row*16 + col;
 
@@ -73,6 +111,7 @@ int min(int row, int col) {
     return min_cell;
 }
 
+// Continuously move to adjacent cell with lowest path cost until wall is detected or center is reached
 int traverse(int current_cell, int* i) {
     int row = current_cell / 16;
     int col = current_cell % 16;
@@ -94,12 +133,14 @@ int traverse(int current_cell, int* i) {
 
 int main() {
     init_layout();
+    curr_dir = RIGHT;
     int current_cell = 0; // start at top left
-    int i = 0;
+    int i = 0; // tracks path length
 
     while(!center_reached(current_cell)) {
         floodfill();
         current_cell = traverse(current_cell, &i);
         print_path();
     }
+    return 0;
 }
